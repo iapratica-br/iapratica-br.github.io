a3bdd67ac628ee2b193f7d1e3f69930a96e66dd7
gugaaf0102-design <gugaaf0102@gmail.com>
2026-08-21 17:15:17 -0300

    Add files via upload

---
+++ kiwify-config.js
@@ -0,0 +1,10 @@
+/*
+  CONFIGURAÇÃO DA KIWIFY
+  1. Crie na Kiwify um produto de pagamento único para o e-book.
+  2. Crie outro produto como "Assinatura recorrente" para a plataforma.
+  3. Cole abaixo os links de checkout copiados em Produtos > Ver links.
+*/
+window.KIWIFY = {
+  ebookCheckoutUrl: "",
+  assinaturaCheckoutUrl: ""
+};
