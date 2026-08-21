4733a7d2638d719bf64f20c17676a724b19d36ed
gugaaf0102-design <gugaaf0102@gmail.com>
2026-08-21 19:13:04 -0300

    Publicar portal da plataforma com login

---
+++ platform-config.js
@@ -0,0 +1,7 @@
+// Preencha apenas com a URL do projeto e a chave pública "anon" do Supabase.
+// Nunca coloque a chave service_role neste arquivo.
+window.IAP_CONFIG = {
+  supabaseUrl: 'https://ijzcyhvhiupoykvshrev.supabase.co',
+  supabaseAnonKey: 'sb_publishable_urSkCjL_gp2r4EZz9-_uFg_fogz5hWx',
+  appUrl: 'mvp.html'
+};
