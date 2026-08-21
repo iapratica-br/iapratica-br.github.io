01e3289741f1a77aa1b7e415e8f7fb12f8bdfca8
gugaaf0102-design <gugaaf0102@gmail.com>
2026-08-21 20:56:48 -0300

    Create painel-dados.js for purchase button links
    Add public configuration for purchase buttons with links.

---
+++ painel-dados.js
@@ -0,0 +1,6 @@
+// Configuração pública dos botões de compra. Não contém chaves ou dados sensíveis.
+window.LINKS_DO_PROGRAMA = {
+  ebook: "https://pay.kiwify.com.br/L4ESQEf",
+  programa: "https://pay.kiwify.com.br/jnEqhsr"
+};
+
