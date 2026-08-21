cc5a1f94d155246660ca174ead1b5662f3e6a4c6
gugaaf0102-design <gugaaf0102@gmail.com>
2026-08-21 20:48:46 -0300

    Create checkout-links.js with public links
    Add public checkout links for ebook and program.

---
+++ checkout-links.js
@@ -0,0 +1,6 @@
+// Links públicos de checkout. Não contém chaves, senhas ou dados sensíveis.
+window.CHECKOUT_LINKS = {
+  ebook: "https://pay.kiwify.com.br/L4ESQEf",
+  programa: "https://pay.kiwify.com.br/jnEqhsr"
+};
+
