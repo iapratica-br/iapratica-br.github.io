4fca4fb7697a8ea4bb475c80f746458603341394
gugaaf0102-design <gugaaf0102@gmail.com>
2026-08-21 17:53:52 -0300

    Set active checkout link for ebook
    Updated the ebookCheckoutUrl with the active checkout link.

--- kiwify-config.js
+++ kiwify-config.js
@@ -1,10 +1,8 @@
 /*
   CONFIGURAÇÃO DA KIWIFY
-  1. Crie na Kiwify um produto de pagamento único para o e-book.
-  2. Crie outro produto como "Assinatura recorrente" para a plataforma.
-  3. Cole abaixo os links de checkout copiados em Produtos > Ver links.
+  O checkout do e-book está ativo. A assinatura da plataforma será adicionada quando estiver disponível.
 */
 window.KIWIFY = {
-  ebookCheckoutUrl: "",
+  ebookCheckoutUrl: "https://pay.kiwify.com.br/L4ESQEf",
   assinaturaCheckoutUrl: ""
 };
